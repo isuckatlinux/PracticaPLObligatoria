7ca7866778517623178e936c34646796ba483de7
pedro <pedroluisgarciamontil@gmail.com>
2022-02-16 09:18:44 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,11 @@ public class Main {
             PrintStream o = new PrintStream(new File("output.html"));
             System.setOut(o);
 
-            mostrarPrincipio();
             CharStream input = CharStreams.fromFileName(args[0]);
             HelloLexer analex = new HelloLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(analex);
             HelloParser anasint = new HelloParser(tokens);
             anasint.r();
-            mostrarFinal();
         } catch (org.antlr.v4.runtime.RecognitionException e) {
             System.err.println("REC " + e.getMessage());
         } catch (IOException e) {
@@ -21,23 +19,6 @@ public class Main {
         } catch (java.lang.RuntimeException e) {
             System.err.println("RUN " + e.getMessage());
         }
-    }
-
-    public static void mostrarPrincipio(){
-        System.out.println("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "   <title>MarkDown2HTML</title>\n" +
-                "<style>\n" +
-                "   .bold {font-weight: bold;}\n" +
-                "   .ital {font-style: italic;} </style>\n" +
-                "</head>\n" +
-                "<body>");
-    }
-
-    public static void mostrarFinal(){
-        System.out.println("</body>\n" +
-                "</html>");
     }
 
 }
